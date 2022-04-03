198f724dfb5feb2e358b73879896a621d3981cec
FengYQ <329650437@qq.com>
2022-04-03 15:05:36 +0800

    Initial Commit

---
+++ Marshell/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Marshell
+//
+//  Created by Edmund Feng on 2022/4/3.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Marshell/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Marshell
+//
+//  Created by Edmund Feng on 2022/4/3.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
